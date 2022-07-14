818a8b28729ff145df0807ec97bcd794e937c627
Jack <jackpar1113@gmail.com>
2022-07-13 23:42:27 -0600

    complete more

--- artichoke.cpp
+++ artichoke.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-#include<cmath>
+#include<math.h>
 
 double helper(int p, int a, int b, int c, int d, int k) {
     return p * (sin(a*k + b) + cos(c * k + d) + 2);
@@ -7,10 +7,22 @@ double helper(int p, int a, int b, int c, int d, int k) {
 
 int main() {
     int p, a, b, c, d, n;
-    int values[n];
+    double values[n];
     std::cin >> p >> a >> b >> c >> d >> n;
 
     for(int i = 0; i < n; i++) {
         values[i] = helper(p, a, b, c, d, i);
     }
+
+    double curr = 0.00;
+    for(int i = 0; i < n-1; i++) {
+        for(int j = i; j < n; j++) {
+            // double val = abs((values[i] - values[j]));
+            // if(val > curr) {
+            //   curr = val;
+            // }
+        }
+    }
+
+    std::cout << curr << std::endl;
 }
